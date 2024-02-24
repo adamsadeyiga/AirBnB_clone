0e61eb2a31e293c5890bf3d323949bc4790ce9d2
Adams Adeyiga <adamsadeyiga1@gmail.com>
2024-02-24 18:32:50 +0100

    Updated

---
+++ AUTHORS.md
@@ -0,0 +1,3 @@
+# This file lists all contributors to the repository.
+
+Adams Adeyiga <alarape3@gmail.com>
---
+++ models/__init__.py
@@ -0,0 +1,8 @@
+# models/__init__.py
+"""
+Module for initializing unique FileStorage instance
+"""
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
